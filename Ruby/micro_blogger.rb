a1e0e7ab5f964eb3dfc901c3006d04b2e01b1100
Roman A. Davis <fluffyribbit@gmail.com>
2014-11-07 11:26:09 +0800

    Added simple server/client

--- Ruby/micro_blogger.rb
+++ Ruby/micro_blogger.rb
@@ -1,11 +1,16 @@
 require "jumpstart_auth"
-
+# PIN: 9152944817
 class MicroBlogger
-
   attr_reader :client
   
   def initialize
     puts "Initialize MicroBlogger"
-    @client = jumpstartAuth.twitter
+    @client = JumpstartAuth.twitter
   end
+  
+  def tweet message
+    @client.update message
+  end
+  blogger = MicroBlogger.new
+  blogger.tweet("MicroBlogger initialized")
 end
