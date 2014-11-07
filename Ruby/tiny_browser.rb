c70730fc8bb6e5f8b1cb80493f4ac1f542c72ad7
Roman A. Davis <fluffyribbit@gmail.com>
2014-11-07 11:54:46 +0800

    Add tiny_browser

---
+++ Ruby/tiny_browser.rb
@@ -0,0 +1,17 @@
+# tiny_browser.rb
+
+require "socket"
+
+host = "www.tutorialspoint.com"
+port = 80
+path = "/index.htm"
+
+# This is the HTTP request  we send to fetch a file
+request = "GET #{path} HTTP/1.0\r\n\r\n"
+
+socket = TCPSocket.open host, port
+
+socket.print request
+response = socket.read
+headers, body = response.split "\r\n\r\n", 2
+print body
